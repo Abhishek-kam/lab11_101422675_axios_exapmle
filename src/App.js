2c3a3e6766bc205be37d9cb55712d3eb8e1e9358
Abhishek-kam <mrfantastff@gmail.com>
2024-11-15 14:17:51 -0500

    lab11

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PersonList from './PersonList';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Axios and Fetch Example</h1>
+            </header>
+            <PersonList />
+        </div>
+    );
+}
+
+export default App;
